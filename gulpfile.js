be572c196063b6ffd6b27ee3666b38a775c0326f
Kristo Godari <kristo.godari@gmail.com>
2015-11-01 02:39:07 +0200

    Commiting simple node.js skeleton application.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/**
+ * Variable declaration
+ * @type {Gulp|exports|module.exports}
+ */
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    port = 8000;
+
+/**
+ * Creating default task for gulp
+ */
+gulp.task('default', function () {
+    nodemon({
+        script: 'bin/www',
+        ext: 'js',
+        env: {
+            PORT: port
+        },
+        ignore: ['./node_modules/**']
+    }).on('restart', function () {
+        console.log('Server restarted and running on port ' + port);
+    });
+});
