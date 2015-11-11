58557374ef6b8c43276fe69edb727a5ca564ce9d
Kristo Godari <kristo.godari@gmail.com>
2015-11-11 23:22:56 +0200

    Creating an simple web crowler. Modifying .gitignore file.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var request = require("request");
+var cheerio = require("cheerio");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    request({
+        uri: "http://www.kristogodari.com",
+    }, function(error, response, body) {
+        var $ = cheerio.load(body);
+
+        $("a").each(function() {
+            var link = $(this);
+            var text = link.text();
+            var href = link.attr("href");
+
+            console.log(text + " -> " + href);
+        });
+    });
 });
 
 module.exports = router;
